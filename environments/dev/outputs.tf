edc6edf59ae6e2012cae52a834f8379ca6b708c7
unknown <faluwoyeo@yahoo.com>
2026-06-14 15:13:05 -0600

    Stage D GitOps Bootstrap

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -16,9 +16,9 @@ output "terraform_deployer_role_arn" {
 }
 
 output "cluster_name" {
-  value = module.eks.cluster_name
+  value = var.enable_eks ? module.eks[0].cluster_name : null
 }
 
 output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
+  value = var.enable_eks ? module.eks[0].cluster_endpoint : null
 }
